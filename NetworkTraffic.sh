d4ee24445c1df17534f0a774598c3e6b2b0d65ff
1440p <38742255+1440p@users.noreply.github.com>
2020-09-19 12:55:07 -0500

    Probably the Final Update for Network.sh
    Overall added $USER and Semicolons, along with comments for documentation

--- NetworkTraffic.sh
+++ NetworkTraffic.sh
@@ -1,32 +1,25 @@
 #!/bin/bash
-pwd
-ls
-cd /home/$USER/Downloads/
-pwd
-ls
-tar -x -f NetworkTraffic.tar.gz
-mkdir WinCrash
-mkdir Scarab
-mkdir Doly\ Trojan
+pwd; ls; cd /home/$USER/Downloads/
+pwd; ls; tar -x -f NetworkTraffic.tar.gz         # cd into Downloads to extract log file
+mkdir WinCrash; mkdir Scarab; mkdir Doly\ Trojan # Makes Three Seperate Directories
+ 
+# Using grep to output mutiple log files along with Numbered Line log files from original log file
 grep -w -e SPT=2583 -e SPT=3024 -e DPT=2583 -e DPT=3024 NetworkTraffic.log > /home/$USER/Downloads/WinCrash/WinCrashPortDump.log
 grep -w -e SPT=1255 -e SPT=1777 -e DPT=1255 -e DPT=1777 NetworkTraffic.log > /home/$USER/Downloads/Scarab/ScarabPortDump.log
 grep -w -e DPT=1010 -e DPT=1011 -e DPT=1012 -e DPT=1015 -e DPT=1016 -e SPT=2345 -e DPT=2345 NetworkTraffic.log > /home/$USER/Downloads/Doly\ Trojan/DolyTrojanPortDump.log
 grep -n -w -e SPT=2583 -e SPT=3024 -e DPT=2583 -e DPT=3024 NetworkTraffic.log > /home/$USER/Downloads/WinCrash/WinCrashPortDumpNumberedLine.log
 grep -n -w -e SPT=1255 -e SPT=1777 -e DPT=1255 -e DPT=1777 NetworkTraffic.log > /home/$USER/Downloads/Scarab/ScarabPortDumpNumberedLine.log
 grep -n -w -e DPT=1010 -e DPT=1011 -e DPT=1012 -e DPT=1015 -e DPT=1016 -e SPT=2345 -e DPT=2345 NetworkTraffic.log > /home/$USER/Downloads/Doly\ Trojan/DolyTrojanPortDumpNumberedLine.log
-cd /home/$USER/Downloads/WinCrash/
-pwd
-ls
-mkdir Daily\ logs
-cd /home/$USER/Downloads/Scarab/
-pwd
-ls
-mkdir Daily\ logs
-cd /home/$USER/Downloads/Doly\ Trojan/
-mkdir Daily\ logs
-cd /home/$USER/Downloads/WinCrash/
-pwd
-ls
+ 
+# Make three directories for daily logs
+cd /home/$USER/Downloads/WinCrash/; pwd; ls; mkdir Daily\ logs
+cd /home/$USER/Downloads/Scarab/; pwd; ls; mkdir Daily\ logs
+cd /home/$USER/Downloads/Doly\ Trojan/; pwd; ls; mkdir Daily\ logs
+ 
+ 
+############### WinCrash ########################################
+# Starts port dumping for one seperate log file; WinCrash only, Ports 2583, 3024
+cd /home/$USER/Downloads/WinCrash/; pwd; ls
 for i in {1..9}
 do
    echo "Making log for May $i"
@@ -39,19 +32,21 @@ do
    grep "May $i" WinCrashPortDump.log > /home/$USER/Downloads/WinCrash/Daily\ logs/May$i.log
    echo "Log for May $i Success"
 done
-cd /home/$USER/Downloads/WinCrash/Daily\ logs/
-pwd
-ls
+ 
+# Starts port dumping for multiple seperate daily log files; WinCrash only
+cd /home/$USER/Downloads/WinCrash/Daily\ logs/; pwd; ls
 wc -l /home/$USER/Downloads/WinCrash/WinCrashPortDump.log > /home/$USER/Downloads/WinCrash/WinCrashCount.log
 for i in {1..27}
 do
-	echo "Counting for WinCrash May $i"
-	wc -l May$i.log >> /home/$USER/Downloads/WinCrash/WinCrashCount.log
+    echo "Counting for WinCrash May $i"
+    wc -l May$i.log >> /home/$USER/Downloads/WinCrash/WinCrashCount.log
 done
-
-cd /home/$USER/Downloads/Scarab/
-pwd
-ls
+############### WinCrash ########################################
+ 
+ 
+############### Scarab ##########################################
+# Starts port dumping for one seperate log file; Scarab only, Ports 1255, 1777
+cd /home/$USER/Downloads/Scarab/; pwd; ls
 for i in {1..9}
 do
    echo "Making log for May $i"
@@ -64,19 +59,21 @@ do
    grep "May $i" ScarabPortDump.log > /home/$USER/Downloads/Scarab/Daily\ logs/May$i.log
    echo "Log for May $i Success"
 done
-cd /home/$USER/Downloads/Scarab/Daily\ logs/
-pwd
-ls
+ 
+# Starts port dumping for multiple seperate daily log files; Scarab only
+cd /home/$USER/Downloads/Scarab/Daily\ logs/; pwd; ls
 wc -l /home/$USER/Downloads/Scarab/ScarabPortDump.log > /home/$USER/Downloads/Scarab/ScarabCount.log
 for i in {1..27}
 do
-	echo "Counting for Scarab May $i"
-	wc -l May$i.log >> /home/$USER/Downloads/Scarab/ScarabCount.log
+    echo "Counting for Scarab May $i"
+    wc -l May$i.log >> /home/$USER/Downloads/Scarab/ScarabCount.log
 done
-
-cd /home/$USER/Downloads/Doly\ Trojan/
-pwd
-ls
+############### Scarab ##########################################
+ 
+ 
+############### Doly Trojan #####################################
+# Starts port dumping for one seperate log file; Doly Trojan only, Ports 1010, 1011, 1012, 1015, 1016, 2345
+cd /home/$USER/Downloads/Doly\ Trojan/; pwd; ls
 for i in {1..9}
 do
    echo "Making log for May $i"
@@ -89,12 +86,13 @@ do
    grep "May $i" DolyTrojanPortDump.log > /home/$USER/Downloads/Doly\ Trojan/Daily\ logs/May$i.log
    echo "Log for May $i Success"
 done
-cd /home/$USER/Downloads/Doly\ Trojan/Daily\ logs/
-pwd
-ls
+ 
+# Starts port dumping for multiple seperate daily log files; Doly Trojan only
+cd /home/$USER/Downloads/Doly\ Trojan/Daily\ logs/; pwd; ls
 wc -l /home/$USER/Downloads/Doly\ Trojan/DolyTrojanPortDump.log > /home/$USER/Downloads/Doly\ Trojan/DolyTrojanCount.log
 for i in {1..27}
 do
-	echo "Counting for Doly Trojan May $i"
-	wc -l May$i.log >> /home/$USER/Downloads/Doly\ Trojan/DolyTrojanCount.log
+    echo "Counting for Doly Trojan May $i"
+    wc -l May$i.log >> /home/$USER/Downloads/Doly\ Trojan/DolyTrojanCount.log
 done
+############### Doly Trojan #######################################
